50ae08fece7479e2576fcba7640960fb6d295665
xudayfi <xudayfiibraahim8@gmail.com>
2023-11-26 09:26:47 +0300

    i ended the daily and weekly bundles

--- functions.dart
+++ functions.dart
@@ -1,5 +1,3 @@
-
-
 code(int codeu){
   if (codeu == 102) {
     print('kusoo dhowoow sometel 4G\n1: Internet\n2: Hadal');
@@ -47,9 +45,9 @@ internetBundle(var bundleChoosed){
 
 
 
-// internet bundle sending 
-internetBundleSending(int sendingBundle){
-  switch (sendingBundle) {
+// internet bundle sending {daily}
+internetBundleSendingDaily(int dailySendingBundle){
+  switch (dailySendingBundle) {
     case 1:
       print('Xirmo Maalinle ah\nGeli lambarka taleefanka aad u furayso 600MB');
       break;
@@ -62,28 +60,99 @@ internetBundleSending(int sendingBundle){
   }
 }
 
-phoneNumberAuthentication(mobileNumber){
+// internet bundle sending {weekly}
+internetBundleSendingWeekly(weeklySendingBundle){
+  switch (weeklySendingBundle) {
+    case 1:
+      print('Xirmo Usbuucle ah\nGeli lambarka taleefanka aad u furayso 500MB');
+      break;
+    case 2:
+      print('Xirmo Usbuucle ah\nGeli lambarka taleefanka aad u furayso 1.5GB');
+      break;
+    case 3:
+      print('Xirmo Usbuucle ah\nGeli lambarka taleefanka aad u furayso 4GB');
+      break;
+    case 4:
+      print('Xirmo Usbuucle ah\nGeli lambarka taleefanka aad u furayso Unlimited(Voice & Data)');
+      break;
+    default: {
+      print('excted');
+    }
+  }
+}
+
+
+phoneNumberAuthentication(mobileNumber, bundleChoosed, dailySendingBundle, weeklySendingBundle){
   if(mobileNumber.length == 9){
-    print('Xirmo Maalinle ah\nMa hubtaa inaad Unlimited (30 saac) oo ah \$0.50 u furto lambarka ah $mobileNumber?\nGeli PIN-kaaga eDahab');
+    if(bundleChoosed == 1){
+      if(dailySendingBundle == 1){
+        print('Xirmo Maalinle ah\nMa hubtaa inaad 600MB oo ah \$0.25 u furto lambarka ah $mobileNumber?\nGeli PIN-kaaga eDahab');
+      } else if(dailySendingBundle == 2){
+        print('Xirmo Maalinle ah\nMa hubtaa inaad Unlimited (30 saac) oo ah \$0.50 u furto lambarka ah $mobileNumber?\nGeli PIN-kaaga eDahab');
+      } else{
+        print('not found');
+      }
+    } else if(bundleChoosed == 2){
+      if(weeklySendingBundle == 1){
+        print('Xirmo Usbuucle ah\nMa hubtaa inaad 500MB oo ah \$0.25 u furto lambarka $mobileNumber?\nGeli PIN-kaaga eDahab');
+      } else if(weeklySendingBundle == 2){
+        print('Xirmo Usbuucle ah\nMa hubtaa inaad 1.5GB oo ah \$0.50 u furto lambarka $mobileNumber?\nGeli PIN-kaaga eDahab');
+      } else if(weeklySendingBundle == 3){
+        print('Xirmo Usbuucle ah\nMa hubtaa inaad 4GB oo ah \$1 u furto lambarka $mobileNumber?\nGeli PIN-kaaga eDahab');
+      } else if(weeklySendingBundle == 4){
+        print('Xirmo Usbuucle ah\nMa hubtaa inaad Unlimited(Voice & Data) oo ah \$2.5 u furto lambarka $mobileNumber?\nGeli PIN-kaaga eDahab');
+      }
+        else{
+        print('wrong');
+      }
   }
   else{
     print('The mobile numer you entered is not a somtel! please try again');
   }
   
 }
+}
 
-// pin authentication
-pinAuthentication(password, mobileNumber){
+
+// pin authentication 
+pinAuthentication(password, mobileNumber, bundleChoosed, dailySendingBundle, weeklySendingBundle){
   int pin = 1234;
   if (password == pin){
-    print('Macmiil, waxaad ku guulaysatay inaad Xirmo Maalinle ah (Unlimited (30 saac)) ugu shubto $mobileNumber. waad ku mahadsantahay isticmaalka adeegyada Somtel!');
+    // daily
+    if(bundleChoosed == 1){
+      switch (dailySendingBundle) {
+        case 1:
+          print('Macmiil, waxaad ku guulaysatay inaad Xirmo Maalinle ah (600MB) ugu shubto $mobileNumber. waad ku mahadsantahay isticmaalka adeegyada Somtel!');
+          break;
+        case 2:
+          print('Macmiil, waxaad ku guulaysatay inaad Xirmo Maalinle ah (Unlimited (30 saac)) ugu shubto $mobileNumber. waad ku mahadsantahay isticmaalka adeegyada Somtel!');
+          break;
+        default:
+          print('exited');
+      }
+      // weekly
+    } else if(bundleChoosed == 2){
+      switch (weeklySendingBundle) {
+        case 1:
+          print('Macmiil, waxaad ku guulaysatay inaad Xirmo Usbuucle ah (500MB) ugu shubto $mobileNumber. waad ku mahadsantahay isticmaalka adeegyada Somtel!');
+          break;
+        case 2:
+          print('Macmiil, waxaad ku guulaysatay inaad Xirmo Usbuucle ah (1.5GB) ugu shubto $mobileNumber. waad ku mahadsantahay isticmaalka adeegyada Somtel!');
+          break;
+        case 3:
+          print('Macmiil, waxaad ku guulaysatay inaad Xirmo Usbuucle ah (4GB) ugu shubto $mobileNumber. waad ku mahadsantahay isticmaalka adeegyada Somtel!');
+          break;
+        case 4:
+          print('Macmiil, waxaad ku guulaysatay inaad Xirmo Usbuucle ah (Unlimited(Voice & Data)) ugu shubto $mobileNumber. waad ku mahadsantahay isticmaalka adeegyada Somtel!');
+          break;
+        default:
+          print('not found');
+      }
+    }
   }
   else{
     print('pinka aad galisay waa khalad fadlan isku day markale (1/3)');
   }
 }
 
-
-//
   
-
--- main.dart
+++ main.dart
@@ -25,29 +25,43 @@ void main(){
   var bundleChoosed = int.parse(bundle ?? '-1');
   internetBundle(bundleChoosed);
 
+  var dailySendingBundle;
+  var weeklySendingBundle;
+
   // sending bundle authentication
-  var senBundle = stdin.readLineSync();
-  var sendingBundle = int.parse(senBundle ?? '-1');
-  internetBundleSending(sendingBundle);
+  if(bundleChoosed == 1){
+    var senBundle = stdin.readLineSync();
+    dailySendingBundle = int.parse(senBundle ?? '-1');
+    internetBundleSendingDaily(dailySendingBundle);
+  }else if(bundleChoosed == 2){
+    var wSendBundle = stdin.readLineSync();
+    weeklySendingBundle = int.parse(wSendBundle ?? '-1');
+    internetBundleSendingWeekly(weeklySendingBundle);
+  }
+  
+
+
 
   // mobile number authenticaton
   var mobileNumber = stdin.readLineSync();
-  phoneNumberAuthentication(mobileNumber);
+  phoneNumberAuthentication(mobileNumber, bundleChoosed, dailySendingBundle, weeklySendingBundle);
+  
+
+
 
   // PIN authentication
-  var passW = stdin.readLineSync();
-  var password = int.parse(passW ?? '-1');
-  pinAuthentication(password, mobileNumber);
+  var mobileNumberLength = mobileNumber.toString().length;
+  if(mobileNumberLength == 9){
+    var passW = stdin.readLineSync();
+    var password = int.parse(passW ?? '-1');
+    pinAuthentication(password, mobileNumber, bundleChoosed, dailySendingBundle, weeklySendingBundle);
+
+  }else{
+    exit(1);
+  }
+  
+
 
   //
-
-
-
-  
-  
-  
-
-
-
 
 }
